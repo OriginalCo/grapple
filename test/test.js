f0463bc837045ec157fd92fef72a362da5015490
Francis Lukesh <fran@lukesh.com>
2013-01-18 10:04:17 -0800

    Just preparing the npm module. Still need to setup testing.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var should = require('should')
+  , schema = require('../')
+  
+describe('define a simple schema', function() {
+  it('should register a Mongoose schema with some primitive datatypes', function(done) {
+    done()
+  })
+
+})
